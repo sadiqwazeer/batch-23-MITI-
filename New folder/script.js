74ea42bd7097bde0356fe771a129396ee879e5ce
sadiq <sadiqwazeer91@gmail.com>
2018-03-02 00:04:59 -0800

    my website

---
+++ New folder/script.js
@@ -0,0 +1,2 @@
+
+document.write("<a href='home.html'>Home</a>| <a href='about.html'>About</a>|<a href='contact.html'>Contact</a>|<a href='gallery.html'>Gallery</a>|<a href='index.html'>index</a>");
